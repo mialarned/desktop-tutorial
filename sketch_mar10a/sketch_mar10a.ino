605e059d9b29d2ba77a55df7d2d6ff132fb0a892
mialarned <mkla243@uky.edu>
2025-03-10 15:12:04 -0400

    3 Lines of Code
    I added 3 lines of code

--- sketch_mar10a/sketch_mar10a.ino
+++ sketch_mar10a/sketch_mar10a.ino
@@ -2,3 +2,9 @@ hi.
 
 
 
+const int PWMB = 10; //speed control pin on the motor driver for the left motor
+const int BIN2 = 9; //control pin 2 on the motor driver for the left motor
+const int BIN1 = 8; //control pin 1 on the motor driver for the left motor
+
+
+
